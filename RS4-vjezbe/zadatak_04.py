4edc884131332d4a46d315a99ff30ed75f272f43
Josip Seketa <jseketa@student.unipu.hr>
2025-12-04 12:58:56 +0100

    Added timeout simulation, code cleanup, comments about race condition.

--- RS4-vjezbe/zadatak_04.py
+++ RS4-vjezbe/zadatak_04.py
@@ -4,8 +4,9 @@ from typing import Dict
 
 # Kako se ponaša asyncio.gather() kada se dogodi iznimka u jednoj od korutina?
 # if the asyncio.gather is kept at its default with return_exception=False
-# the firs exception is propagated, and all the other coroutines are cancelled
+# the first exception is propagated, and all the other coroutines are cancelled
 # if still pending
+#
 # if I want to collect exceptions instead of panicking out of it,
 # use asyncio.gather(*task_list, return_exceptions=True)
 
@@ -15,12 +16,30 @@ korisnici: Dict[str, str] = {
     "korisnik3": "lozinka3",
 }
 
+# 5 users: some valid, some invalid
+attempts = [
+    ("korisnik1", "lozinka1"),  # valid
+    ("korisnik2", "wrong"),  # invalid
+    ("korisnikX", "lozinka3"),  # invalid (user not in dict)
+    ("korisnik3", "lozinka3"),  # valid
+    ("korisnik2", "lozinka2"),  # valid
+]
 
-async def autentifikacija(korisnicko_ime: str, lozinka: str) -> bool:
+
+async def autentifikacija(
+    korisnicko_ime: str, lozinka: str, service_down: bool = False
+) -> bool:
     """
     Simulate slow I/O (2 seconds) and then check username/password.
     Return True if correct, otherwise raise ValueError.
     """
+    # simulate timeout
+    # not really happy with this because of the race condition
+    # between this raise and wait_for raise
+    if service_down:
+        await asyncio.sleep(3)
+        raise TimeoutError("Service is down.")
+
     print(f"Starting auth for {korisnicko_ime}")
     await asyncio.sleep(2)
 
@@ -32,24 +51,21 @@ async def autentifikacija(korisnicko_ime: str, lozinka: str) -> bool:
     raise ValueError(f"Invalid credentials for {korisnicko_ime}")
 
 
-async def main_basic() -> None:
-    # 5 users: some valid, some invalid
-    attempts = [
-        ("korisnik1", "lozinka1"),  # valid
-        ("korisnik2", "wrong"),  # invalid
-        ("korisnikX", "lozinka3"),  # invalid (user not in dict)
-        ("korisnik3", "lozinka3"),  # valid
-        ("korisnik2", "lozinka2"),  # valid
+async def main() -> None:
+    # tasks are already running here
+    tasks = [
+        asyncio.wait_for(autentifikacija(user, password, True), timeout=3.1)
+        for user, password in attempts
     ]
+    # if the timeout error occurs, there is a race condition between
+    # the TimeoutError exception raised in the coroutine and the one
+    # that the asyncio.wait_for raises.
+    # only happens if the wait timeout and sleep are the same value
 
-    coros = [autentifikacija(u, p) for u, p in attempts]
-
-    try:
-        results = await asyncio.gather(*coros)
-        print("Results:", results)
-    except Exception as e:
-        print("asyncio.gather raised:", repr(e))
+    # gathering the results
+    results = await asyncio.gather(*tasks, return_exceptions=True)
+    print("Results:", results)
 
 
 if __name__ == "__main__":
-    asyncio.run(main_basic())
+    asyncio.run(main())
