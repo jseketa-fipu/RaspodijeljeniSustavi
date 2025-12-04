8270239a9b08ee89bd3ad2eb2e53837632083079
Josip Seketa <jseketa@student.unipu.hr>
2025-12-04 20:33:44 +0100

    Removed wait_for, but left the reference in the comment about the race condition.

--- RS4-vjezbe/zadatak_04.py
+++ RS4-vjezbe/zadatak_04.py
@@ -53,10 +53,7 @@ async def autentifikacija(
 
 async def main() -> None:
     # tasks are already running here
-    tasks = [
-        asyncio.wait_for(autentifikacija(user, password, True), timeout=3.1)
-        for user, password in attempts
-    ]
+    tasks = [autentifikacija(user, password, False) for user, password in attempts]
     # if the timeout error occurs, there is a race condition between
     # the TimeoutError exception raised in the coroutine and the one
     # that the asyncio.wait_for raises.
