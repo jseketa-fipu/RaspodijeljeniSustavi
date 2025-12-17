47f4472731e4f3664d8755922d40cf2fe5f5e6c5
Josip Seketa <jseketa@student.unipu.hr>
2025-12-17 23:08:44 +0100

    Added kolicina to the list of products.

--- RS5-vjezbe/zadatak_1.py
+++ RS5-vjezbe/zadatak_1.py
@@ -2,11 +2,11 @@ from aiohttp import web
 
 # Hard-coded set of products that the service returns.
 PROIZVODI: list[dict[str, int | str]] = [
-    {"id": 1, "naziv": "Laptop", "cijena": 5000},
-    {"id": 2, "naziv": "Miš", "cijena": 100},
-    {"id": 3, "naziv": "Tipkovnica", "cijena": 200},
-    {"id": 4, "naziv": "Monitor", "cijena": 1000},
-    {"id": 5, "naziv": "Slušalice", "cijena": 50},
+    {"id": 1, "naziv": "Laptop", "cijena": 5000, "kolicina": 2},
+    {"id": 2, "naziv": "Miš", "cijena": 100, "kolicina": 15},
+    {"id": 3, "naziv": "Tipkovnica", "cijena": 200, "kolicina": 12},
+    {"id": 4, "naziv": "Monitor", "cijena": 1000, "kolicina": 7},
+    {"id": 5, "naziv": "Slušalice", "cijena": 50, "kolicina": 100},
 ]
 
 
